2576d09be2f5d84196f4d8251383e0827827742b
Frank <frank.permenter@tri.global>
2020-11-04 13:03:08 -0500

    Added test cases.

---
+++ conex/test/matrix_exponential_test.cc
@@ -0,0 +1,34 @@
+#include "gtest/gtest.h"
+
+#include "conex/matrix_exponential.h"
+
+#include <Eigen/Dense>
+#include <unsupported/Eigen/MatrixFunctions>
+
+#include "conex/debug_macros.h"
+
+using Eigen::MatrixXd;
+using Eigen::Map;
+using Eigen::VectorXd;
+
+TEST(MatrixExponential, CompareWithEigen) {
+  int n = 4;
+  MatrixXd A(n, n); 
+  A << 3, 1, 0, 1,
+       1, 3, 1, 0,
+       0, 1, 4, 1,
+       1, 0, 1, 5;
+  A = A / A.trace();
+
+  MatrixXd reference = A.exp();
+  MatrixXd calculated(n, n);
+      
+  Map<MatrixXd, Eigen::Aligned> map(calculated.data(), n, n);
+  MatrixExponential(A, &map);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      EXPECT_NEAR(reference(i, j), calculated(i, j), 1e-7);
+    }
+  }
+}
+
---
+++ conex/test/test_warmstart.cc
@@ -0,0 +1,66 @@
+#include "gtest/gtest.h"
+#include <Eigen/Dense>
+
+#include "conex/linear_constraint.h"
+#include "conex/dense_lmi_constraint.h"
+#include "conex/cone_program.h"
+#include "conex/test/test_util.h"
+
+using Eigen::MatrixXd;
+
+TEST(Warmstart, AgreesWithFullSolveIfNoDataIsChanged) {
+  SolverConfiguration config;
+  config.inv_sqrt_mu_max = 10000000;
+  config.final_centering_steps = 0;
+  int num_iters = 10;
+  int n = 15;
+  int m = 13;
+  auto constraints2 = GetRandomDenseMatrices(n, m);
+
+  DenseMatrix affine2 = Eigen::MatrixXd::Identity(n, n);
+  DenseLMIConstraint LMI{n, constraints2, affine2};
+
+  Program prog;
+  DenseMatrix y(m, 1);
+  prog.constraints.push_back(LMI);
+
+  auto b = GetFeasibleObjective(m, prog.constraints);
+  config.max_iterations = num_iters;
+  Solve(b, prog, config, y.data());
+
+  DenseMatrix ywarm(m, 1);
+  for (int i = 0; i < num_iters; i++) {
+    config.initialization_mode = i != 0;
+    config.max_iterations = 1;
+    config.final_centering_steps = 0;
+    Solve(b, prog, config, ywarm.data());
+  }
+  EXPECT_NEAR((y - ywarm).norm(), 0, 1e-12);
+}
+/*
+TEST(Warmstart, ObjectivePertubation) {
+  SolverConfiguration config;
+  config.inv_sqrt_mu_max = 1000;
+  config.final_centering_steps = 0;
+  config.divergence_upper_bound = 10000;
+  int num_iters = 20;
+  int n = 15;
+  int m = 13;
+  auto constraints2 = GetRandomDenseMatrices(n, m);
+
+  DenseMatrix affine2 = Eigen::MatrixXd::Identity(n, n);
+  DenseLMIConstraint LMI{n, constraints2, affine2};
+
+  Program prog;
+  DenseMatrix y(m, 1);
+  prog.constraints.push_back(LMI);
+
+  auto b = GetFeasibleObjective(m, prog.constraints);
+  config.max_iterations = num_iters;
+  Solve(b, prog, config, y.data());
+
+  b = b + MatrixXd::Random(m, 1) * .05;
+  DenseMatrix ywarm(m, 1);
+  config.initialization_mode = 1;
+  Solve(b, prog, config, ywarm.data());
+}*/
