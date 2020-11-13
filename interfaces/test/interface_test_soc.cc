acd9f19eb144e71f33f5c99651b05ba191231a47
Frank <frank.permenter@tri.global>
2020-11-13 16:10:23 -0500

    Add interface test unit tests.

---
+++ interfaces/test/interface_test_soc.cc
@@ -0,0 +1,62 @@
+#include "interfaces/conex.h"
+#include "conex/debug_macros.h"
+
+#include "gtest/gtest.h"
+
+TEST(TestArguments, AddLMI) {
+  void* p = ConexCreateConeProgram();
+  int constraint_id = 0;
+  EXPECT_TRUE(CONEX_NewLorentzConeConstraint(p, 2, &constraint_id) == CONEX_SUCCESS);
+  EXPECT_EQ(0, constraint_id);
+  EXPECT_TRUE(CONEX_NewLorentzConeConstraint(p, 2, &constraint_id) == CONEX_SUCCESS);
+  EXPECT_EQ(1, constraint_id);
+
+  void* null_ptr = NULL;
+  EXPECT_TRUE(CONEX_NewLorentzConeConstraint(null_ptr,  2, &constraint_id) == CONEX_FAILURE);
+
+  void* corrupted_ptr = static_cast<double*>(p) + 1;
+  EXPECT_TRUE(CONEX_NewLorentzConeConstraint(corrupted_ptr,  2, &constraint_id) == CONEX_FAILURE);
+
+  int bad_order = 0;
+  EXPECT_TRUE(CONEX_NewLorentzConeConstraint(p, bad_order, &constraint_id) == CONEX_FAILURE);
+  ConexDeleteConeProgram(p);
+}
+
+TEST(TestArguments, UpdateLMI) {
+  void* p = ConexCreateConeProgram();
+
+  int status;
+  int constraint_id = 0;
+  int order = 2;
+
+  status = CONEX_NewLorentzConeConstraint(p, order,  &constraint_id);
+  EXPECT_EQ(CONEX_SUCCESS, status);
+  
+  status = CONEX_UpdateLinearOperator(p, constraint_id, .3, 2, order - 1, 0, 0);
+  EXPECT_EQ(CONEX_SUCCESS, status);
+
+  int bad_hyper_complex_dim = 1; 
+  int bad_variable = -1;
+  int bad_column_index = 1;
+
+  status = CONEX_UpdateLinearOperator(p, constraint_id, .3, 2, order - 1, 0, bad_hyper_complex_dim);
+  EXPECT_EQ(CONEX_FAILURE, status); 
+  status = CONEX_UpdateLinearOperator(p, constraint_id, .3, 2, bad_variable, 0, 0);
+  EXPECT_EQ(CONEX_FAILURE, status); 
+  status = CONEX_UpdateLinearOperator(p, constraint_id, .3, 2, order - 1, bad_column_index, 0);
+  EXPECT_EQ(CONEX_FAILURE, status); 
+
+
+  // Repeat for affine term.
+  status = CONEX_UpdateAffineTerm(p, constraint_id, .3, 0, 0, 0);
+  EXPECT_EQ(CONEX_SUCCESS, status); 
+  status = CONEX_UpdateAffineTerm(p, constraint_id, .3, 2, 0, 0);
+  EXPECT_EQ(CONEX_SUCCESS, status); 
+
+  status = CONEX_UpdateAffineTerm(p, constraint_id, .3, 2, 0, bad_hyper_complex_dim);
+  EXPECT_EQ(CONEX_FAILURE, status); 
+  status = CONEX_UpdateAffineTerm(p, constraint_id, .3, 2, bad_column_index, 0);
+  EXPECT_EQ(CONEX_FAILURE, status); 
+
+  ConexDeleteConeProgram(p);
+}
