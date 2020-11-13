1c05a13e13d26c5bff65a2c0e293a15c94fe0a08
Frank <frank.permenter@tri.global>
2020-11-13 16:09:10 -0500

    Add error checking macros

---
+++ conex/error_checking_macros.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "conex/error_codes.h"
+
+// TODO(FrankPermenter): return conex error codes on failure.
+
+
+#if 0
+#define CONEX_DEMAND(x, msg) \
+    if (!(x)) { \
+      std::cout << "Conex error: " << msg << std::endl; \
+      return 1; \
+    } 
+#else
+#define CONEX_DEMAND(x, msg) \
+    if (!(x)) { \
+            std::cout << __FILE__  << " line " << __LINE__ \
+                      << ": " <<  msg  << std::endl;\
+      return 1; \
+    } 
+#endif
---
+++ conex/error_codes.h
@@ -0,0 +1,2 @@
+#define CONEX_SUCCESS 0
+#define CONEX_FAILURE 1
