881154074e01431933b902ce228dec7704aa5b39
Frank <frank.permenter@tri.global>
2021-01-07 14:15:59 -0500

    Added failing MPC example.

--- conex/test/solver_failures.cc
+++ conex/test/solver_failures.cc
@@ -1,6 +1,7 @@
 #include "conex/cone_program.h"
 #include "conex/equality_constraint.h"
 #include "conex/linear_constraint.h"
+#include "conex/quadratic_cost.h"
 #include <Eigen/Dense>
 
 namespace conex {
@@ -42,6 +43,104 @@ void EqualityConstraintFailingLDLT() {
   DUMP(eq * solution - eq_affine);
   DUMP(solution);
 }
+
+class MPCFailingLDLT {
+ public:
+  template <typename T>
+  using vector = std::vector<T>;
+  static constexpr int T = 3;
+  static constexpr int nu = 1;
+  static constexpr int nx = 2;
+
+  auto InputVars(int i) {
+    int offset = T * nx + i * nu;
+    // x, u x u
+    vector<int> y;
+    for (int i = 0; i < nu; i++) {
+      y.push_back(i + offset);
+    }
+    return y;
+  }
+
+  auto StateVars(int i) {
+    assert(i >= 1);
+    int offset = (i - 1) * nx;
+    vector<int> y;
+    for (int i = 0; i < nx; i++) {
+      y.push_back(i + offset);
+    }
+    return y;
+  }
+
+  auto StageVars(int i, bool next_state) {
+    vector<int> y;
+    if (i > 0) {
+      for (const auto& c : StateVars(i)) {
+        y.push_back(c);
+      }
+    }
+
+    for (const auto& c : InputVars(i)) {
+      y.push_back(c);
+    }
+
+    for (const auto& c : StateVars(i + 1)) {
+      y.push_back(c);
+    }
+    return y;
+  }
+
+  auto DynamicsConstraint(const MatrixXd& Ai, const MatrixXd& Bi, int i) {
+    if (i > 0) {
+      MatrixXd A(nx, 2 * nx + nu);
+      A << Ai, Bi, -MatrixXd::Identity(nx, nx);
+      return A;
+    } else {
+      MatrixXd A(nx, nx + nu);
+      A << Bi, -MatrixXd::Identity(nx, nx);
+      return A;
+    }
+  }
+
+  void Run(bool fail) {
+    int num_vars = T * (nu + nx + 2);
+    int epigraph_start = T * (nu + nx);
+
+    MatrixXd Ai = MatrixXd::Random(nx, nx);
+    MatrixXd Bi = MatrixXd::Random(nx, nu);
+    MatrixXd f = MatrixXd::Random(nx, 1);
+    MatrixXd Hxu = MatrixXd::Random(2, nu);
+    MatrixXd gxu = MatrixXd::Random(2, 1);
+
+    conex::Program prog(num_vars);
+    for (int i = 0; i < T; i++) {
+      MatrixXd M(nx, nu + 2 * nx);
+      prog.AddConstraint(
+          conex::EqualityConstraints{DynamicsConstraint(Ai, Bi, i), f},
+          StageVars(i, true));
+
+      if (fail) {
+        if (i > 0 && i < T - 1) {
+          prog.AddConstraint(conex::LinearConstraint{Hxu, gxu},
+                             StageVars(i, false));
+        }
+      }
+
+      AddQuadraticCost(&prog, MatrixXd::Identity(nu, nu), InputVars(i),
+                       epigraph_start++);
+      AddQuadraticCost(&prog, MatrixXd::Identity(nx, nx), StateVars(i + 1),
+                       epigraph_start++);
+    }
+
+    Eigen::VectorXd var(num_vars);
+    VectorXd linear_cost(num_vars);
+    conex::Solve(linear_cost, prog, conex::SolverConfiguration(), var.data());
+  }
+};
+
 }  // namespace conex
 
-int main() { conex::EqualityConstraintFailingLDLT(); }
+int main() {
+  conex::EqualityConstraintFailingLDLT();
+  conex::MPCFailingLDLT().Run(true /*trigger fail*/);
+}
