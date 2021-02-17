bef751031786cf0a9da97fa46f8bb3e6509e3e0e
Frank <frank.permenter@tri.global>
2021-02-17 12:12:28 -0500

    Added sparse equation test.

--- conex/test/equality_constraints_test.cc
+++ conex/test/equality_constraints_test.cc
@@ -49,4 +49,70 @@ GTEST_TEST(EqualityConstraints, Basic) {
   EXPECT_NEAR((eq * solution - eq_affine).norm(), 0, 1e-5);
   EXPECT_NEAR((solution - optimal_y).norm(), 0, 1e-5);
 }
+
+void DoManySeparate() {
+  int num_vars = 10;
+  int num_inequalities = num_vars + 10;
+  int num_equalities = num_vars - 2;
+
+  MatrixXd A = MatrixXd::Random(num_inequalities, num_vars);
+  MatrixXd C(num_inequalities, 1);
+
+  VectorXd optimal_slack(num_inequalities);
+  VectorXd optimal_dual(num_inequalities);
+  VectorXd optimal_y(num_vars);
+
+  optimal_slack.setConstant(1);
+  optimal_dual.setConstant(1);
+  int m = num_inequalities * .5;
+  optimal_slack.topRows(m).setConstant(1e-7);
+  optimal_dual.bottomRows(num_inequalities - m).setConstant(1e-7);
+
+  optimal_y = Eigen::MatrixXd::Random(num_vars, 1);
+
+  C = optimal_slack + A * optimal_y;
+
+  LinearConstraint linear_inequality{A, C};
+
+  Program prog(num_vars);
+  MatrixXd eq = MatrixXd::Zero(num_equalities, num_vars);
+  Eigen::MatrixXd Bi(1, 3);
+  Bi << 1, 2, 3;
+  for (int i = 0; i < num_equalities; i++) {
+    std::vector<int> vars{0, i + 1, num_vars - 1};
+    for (size_t j = 0; j < vars.size(); j++) {
+      eq(i, vars.at(j)) = Bi(0, j);
+    }
+    prog.AddConstraint(EqualityConstraints{Bi, eq.row(i) * optimal_y}, vars);
+  }
+
+  MatrixXd eq_affine(num_equalities, 1);
+  eq_affine = eq * optimal_y;
+
+  prog.AddConstraint(linear_inequality);
+
+  VectorXd linear_cost(num_vars);
+  linear_cost = A.transpose() * optimal_dual;
+
+  VectorXd solution(num_vars);
+  auto config = conex::SolverConfiguration();
+  config.final_centering_steps = 10;
+
+  config.initial_centering_steps_coldstart = 0;
+  config.max_iterations = 40;
+  config.divergence_upper_bound = .5;
+  Solve(linear_cost, prog, config, solution.data());
+
+  EXPECT_TRUE((C - A * solution).minCoeff() > -1e8);
+  EXPECT_NEAR((eq * solution - eq_affine).norm(), 0, 1e-7);
+  EXPECT_GE(linear_cost.dot(solution) + 1e-4, linear_cost.dot(optimal_y));
+}
+
+GTEST_TEST(EqualityConstraints, ManySeparate) {
+  srand(0);
+  for (int i = 0; i < 10; i++) {
+    DoManySeparate();
+  }
+}
+
 }  // namespace conex
