5b2147510e2ff7d308c7735c50610163f84a554e
Frank <frank.permenter@tri.global>
2021-04-24 21:46:46 -0400

    Move functions out of header.

--- conex/triangular_matrix_workspace.cc
+++ conex/triangular_matrix_workspace.cc
@@ -34,6 +34,67 @@ double* LookupAddress(T& o, int r, int c) {
 
 }  // namespace
 
+TriangularMatrixWorkspace::TriangularMatrixWorkspace(
+    const std::vector<Clique>& path_, const std::vector<int>& supernode_size_)
+    : supernode_size(supernode_size_) {
+  N = std::accumulate(supernode_size.begin(), supernode_size.end(), 0);
+  variable_to_supernode_.resize(N);
+  variable_to_supernode_position_.resize(N);
+
+  separators.resize(path_.size());
+  int cnt = 0;
+  for (auto& si : separators) {
+    for (size_t i = supernode_size.at(cnt); i < path_.at(cnt).size(); i++) {
+      si.push_back(path_.at(cnt).at(i));
+    }
+    cnt++;
+  }
+
+  cnt = 0;
+  int var = 0;
+  snodes.resize(path_.size());
+  for (auto& si : snodes) {
+    si.resize(supernode_size.at(cnt));
+    for (int i = 0; i < supernode_size.at(cnt); i++) {
+      if (var >= N) {
+        std::runtime_error("Invalid variable index.");
+      }
+      si.at(i) = path_.at(cnt).at(i);
+      variable_to_supernode_[var] = cnt;
+      variable_to_supernode_position_[var] = i;
+      var++;
+    }
+    cnt++;
+  }
+}
+
+void Initialize(TriangularMatrixWorkspace* o, double* data_start) {
+  double* data = data_start;
+  for (size_t j = 0; j < o->snodes.size(); j++) {
+    o->diagonal.emplace_back(data, o->supernode_size.at(j),
+                             o->supernode_size.at(j));
+
+    data += o->SizeOfSupernode(j);
+    o->off_diagonal.emplace_back(data, o->supernode_size.at(j),
+                                 o->separators.at(j).size());
+    data += o->SizeOfSeparator(j);
+  }
+
+  o->seperator_diagonal.resize(o->snodes.size());
+  for (size_t j = 0; j < o->snodes.size(); j++) {
+    o->S_S(j, &o->seperator_diagonal.at(j));
+  }
+  o->SetIntersections();
+
+  // Use reserve so that we can call default constructor of LLT objects.
+  o->llts.reserve(o->snodes.size());
+
+  o->temporaries.resize(o->snodes.size());
+  for (size_t j = 0; j < o->snodes.size(); j++) {
+    o->temporaries.at(j).resize(o->separators.at(j).size());
+  }
+}
+
 void TriangularMatrixWorkspace::S_S(int clique, std::vector<double*>* y) {
   auto& s = separators.at(clique);
   int size = .5 * (s.size() * s.size() + s.size());
--- conex/triangular_matrix_workspace.h
+++ conex/triangular_matrix_workspace.h
@@ -13,40 +13,7 @@ using Clique = std::vector<int>;
 
 struct TriangularMatrixWorkspace {
   TriangularMatrixWorkspace(const std::vector<Clique>& path_,
-                            const std::vector<int>& supernode_size_)
-      : supernode_size(supernode_size_) {
-
-    N = std::accumulate(supernode_size.begin(), supernode_size.end(), 0);
-    variable_to_supernode_.resize(N);
-    variable_to_supernode_position_.resize(N);
-
-    separators.resize(path_.size());
-    int cnt = 0;
-    for (auto& si : separators) {
-      for (size_t i = supernode_size.at(cnt); i < path_.at(cnt).size(); i++) {
-        si.push_back(path_.at(cnt).at(i));
-      }
-      cnt++;
-    }
-
-    cnt = 0;
-    int var = 0;
-    snodes.resize(path_.size());
-    for (auto& si : snodes) {
-      si.resize(supernode_size.at(cnt));
-      for (int i = 0; i < supernode_size.at(cnt); i++) {
-        if (var >= N) {
-          std::runtime_error("Invalid variable index.");  
-        }
-        si.at(i) = path_.at(cnt).at(i);
-        variable_to_supernode_[var] = cnt;
-        variable_to_supernode_position_[var] = i;
-        var++;
-      }
-      cnt++;
-    }
-
-  }
+                            const std::vector<int>& supernode_size_);
   int N;
   // TODO(FrankPermenter): Remove all of these members.
   std::vector<int> supernode_size;
@@ -76,32 +43,7 @@ struct TriangularMatrixWorkspace {
     return size;
   }
 
-  friend void Initialize(TriangularMatrixWorkspace* o, double* data_start) {
-    double* data = data_start;
-    for (size_t j = 0; j < o->snodes.size(); j++) {
-      o->diagonal.emplace_back(data, o->supernode_size.at(j),
-                               o->supernode_size.at(j));
-
-      data += o->SizeOfSupernode(j);
-      o->off_diagonal.emplace_back(data, o->supernode_size.at(j),
-                                   o->separators.at(j).size());
-      data += o->SizeOfSeparator(j);
-    }
-
-    o->seperator_diagonal.resize(o->snodes.size());
-    for (size_t j = 0; j < o->snodes.size(); j++) {
-      o->S_S(j, &o->seperator_diagonal.at(j));
-    }
-    o->SetIntersections();
-
-    // Use reserve so that we can call default constructor of LLT objects.
-    o->llts.reserve(o->snodes.size());
-
-    o->temporaries.resize(o->snodes.size());
-    for (size_t j = 0; j < o->snodes.size(); j++) {
-      o->temporaries.at(j).resize(o->separators.at(j).size());
-    }
-  }
+  friend void Initialize(TriangularMatrixWorkspace* o, double* data_start);
 
   // Find the points in both separator(j) and supernode(i) for i + 1 > j.
   // For each point, returns the position in the separator and the
@@ -122,11 +64,11 @@ struct TriangularMatrixWorkspace {
 
   std::vector<int> variable_to_supernode_;
   std::vector<int> variable_to_supernode_position_;
+
  private:
   void SetIntersections();
   // TODO(FrankPermenter): Remove this method.
   void S_S(int clique, std::vector<double*>*);
-
 };
 
 }  // namespace conex
